93533e5a8363bf159997c53b3b2c6f2b6b14898c
ljimenezhi <ljimenezhi@vectoritcgroup.com>
2019-07-12 19:14:01 -0500

    responsive 8

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // $(document).on('ready', function(){
     $(".fb-button").on('click', function(e) {
         e.preventDefault();
-        $(".chat-content").toggleClass('openChat')
+        $(".chat-fb .chat-content").toggleClass('openChat')
     })
+
+    $(".wsp-button").on('click', function(e) {
+        e.preventDefault();
+        $(".chat-wsp .chat-content").toggleClass('openChatWsp');
+        $(".btn-enviar-wsp").toggleClass('index-wsp')
+    })
+
 
     setTimeout(() => {
         $('.splash').css('display', 'none');
